b2ff11fb383a2e6fc0af4e90a9339c590511c9f9
Barbarakovacs85 <133600947+Barbarakovacs85@users.noreply.github.com>
2024-04-24 14:33:48 +0200

    correction

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import Display from './Display';
-import Buttons from './Buttons';
+import Display from './components/Display';
+import Buttons from './components/Buttons';
 import './App.css';
 
 function App() {
   const [total, setTotal] = useState('');
+  const [lastButton, setlastButton] = useState('0');
 
   const handleClick = (e) => {
-    setTotal(total.concat(e.target.name));
+    setlastButton(e.target.name);
+    
+    setTotal(lastButton == null ? 0: total.concat(e.target.name));
   }
   const clear = () => {
     setTotal("");
@@ -30,7 +33,7 @@ function App() {
       <h1>Számológép</h1>
 
       <div className="container">
-        
+        {lastButton}
         <Display total={total} />
         <Buttons clear={clear} handleDelete={handleDelete} handleClick={handleClick} calculate={calculate} />
       </div>
